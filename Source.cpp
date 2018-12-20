595b78048420e5c1cdede3abb4dc1fa202335026
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-20 13:03:56 +0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,17 +1,30 @@
+/**
+* @file Source.cpp
+*
+* @brief This code tests Composition of Employee class.
+* Employee class is composed from Date and Address classes.
+*
+* @author Saif Ullah Ijaz
+*
+*/
 #include<iostream>
+using namespace std;
 #include "Employee.h"
 
+// function main begins program execution
 void main()
 {
-	Employee e1("Saif Ullah Ijaz", "AD1673333", Date(1,1,1986), Date(28,1,2014), Address(1857, "Some street", "Lahore", "Punjab", 4500), 0.40, 30000);
+	cout << "Employee count: " << Employee::sGetTotalEmployee() << endl;
+	Employee e1("Saif Ullah Ijaz", "AD1673333", Date(1, 1, 1986), Date(28, 1, 2014), Address(1857, "Some street", "Lahore", "Punjab", 4500), 0.40, 30000);
 	cout << "Employee 1: " << endl;
 	cout << e1;
 	Employee e2 = e1;
 	cout << "Employee 2: " << endl;
 	cout << e2 << endl;
-	e2.setZIP(37460);
-	cout << "Employee2 zipcode: " << e2.getZIP() << endl;
-	cout << "Employee2 net salary: " << e2.calcSalary() << endl;
-
+	e2.SetZIP(37460);
+	cout << "Employee2 zipcode: " << e2.GetZIP() << endl;
+	cout << "Employee2 net salary: " << e2.CalcSalary() << endl;
+	cout << "Employee count: " << Employee::sGetTotalEmployee() << endl;
 	system("pause");
 }
+// end main
