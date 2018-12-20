44b6f882096b7cad8f7fe196ca0a57b860854f00
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-20 13:02:56 +0800

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -1,55 +1,141 @@
-#ifndef Employee_HEADER 
-#define Employee_HEADER
-#include <iostream>
+/**  Employee class as an example of Composition.
+*
+* #include "Employee.h" <BR>
+* -llib
+*
+*/
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+// SYSTEM INCLUDES
+#include<iostream>
+using std::string;
+
+// LOCAL INCLUDES
 #include"Date.h"
 #include"Address.h"
-using namespace std;
+
+
 // Employee class definition 
 class Employee {
-	string Name;
-	string NIC;
-	const Date BIRTH_DATE;
-	const Date HIRE_DATE;
-	Address eAddress;
-	double taxRate;
-	double basicSalary;
-
-	static int noOfEmployees;		// class variable to count total no. of employees
-	bool checkName(string)const;		// utility function to check valid name
-	bool checkNIC(string)const;		// utility function to check valid NIC
 public:
-	Employee(string = "", string = "", const Date & = Date::getDefaultDate(), const Date & = Date::getDefaultDate(), Address & = Address::getDefaultAddress(), double = 0.0, double = 0.0); // overloaded constructor
-	Employee(const Employee &);		//copy constructor
-	~Employee(); // destructor
+// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	Employee(const string& = "", const string& = "", const Date& = Date::sGetDefaultDate(), const Date& = Date::sGetDefaultDate(), const Address& = Address::sGetDefaultAddress(), double = 0.0, double = 0.0);
 
-	friend ostream & operator <<(ostream &, const Employee &);		// Stream Insertion
-	friend istream & operator >>(istream &, Employee &);			// Stream Extraction
+	/** Copy constructor.
+	*/
+	Employee(const Employee&);
 
+	/** destructor.
+	*/
+	~Employee();
+
+	// Use compiler-generated assignment.
+	// Date& operator=(const Date&);
+
+// OPERATORS
+	/** Stream Insertion operator.
+	*
+	* @param os Standard Output Stream.
+	* @param from The value to be inserted to the output stream.
+	*
+	* @return A reference to the standard output stream.
+	*/
+	friend std::ostream& operator <<(std::ostream& os, const Employee& from);
+
+	/** Stream Extraction operator.
+	*
+	* @param is Standard Intput Stream.
+	* @param to The value to be extracted from the input stream.
+	*
+	* @return A reference to the standard input stream.
+	*/
+	friend std::istream& operator >>(std::istream& is, Employee& to);
+
+// OPERATIONS
+	/** function that prints all the details of the employee.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void PrintEmployee()const;
+
+	/** function that calculates net salary of the employee.
+	*
+	* @param void
+	*
+	* @return net salary
+	*/
+	double CalcSalary();
+
+// ACCESS
 	// setters
-	void setName(string);
-	void setNIC(string);
-	void setAddress(int = 0, string = "", string = "", string = "", int = 0);
-	void setAddress(const Address &);
-	void setTaxRate(double = 0.0);
-	void setBasicSalary(double = 0.0);
-	void setEmployee(string, string, const Address &, double, double);
-	void setEmployee(const Employee &);
-	void setZIP(int = 0); // Setter function for Postal(ZIP) Code
+	void SetName(const string& = "");
+	void SetNIC(const string& = "");
+	void SetHomeAddress(int = 0, const string& = "", const string& = "", const string& = "", int = 0);
+	/**
+	# @overload void SetHomeAddress(const Address& aAddress);
+	*/
+	void SetHomeAddress(const Address& aAddress);
+	// Setter function for Postal(ZIP) Code
+	void SetZIP(int = 0);
+	void SetTaxRate(double = 0.0);
+	void SetBasicSalary(double = 0.0);
+	void SetEmployee(const string& = "", const string& = "", const Address& = Address::sGetDefaultAddress(), double = 0.0, double = 0.0);
+	/**
+	# @overload void SetEmployee(const Employee& aEmployee);
+	*/
+	void SetEmployee(const Employee& aEmployee);
+	
 	
 	// getters
-	string getName()const;
-	string getNIC()const;
-	const Date & getDOB()const;
-	const Date & getDOJ()const;
-	const Address & getAddress()const;
-	double getTaxRate()const;
-	double getBasicSalary()const;
-	int getZIP()const; // Getter function for Postal(ZIP) Code 
-	void PrintEmployee()const; // prints all the details of the employee 
+	const string& GetName()const;
+	const string& GetNIC()const;
+	const Date& GetDOB()const;
+	const Date& GetDOJ()const;
+	const Address& GetHomeAddress()const;
+	// Getter function for Postal(ZIP) Code
+	int GetZIP()const; 
+	double GetTaxRate()const;
+	double GetBasicSalary()const;
+	const Employee& GetEmployee()const;
+	static int sGetTotalEmployee();
 
-	static int getTotalEmployee(); // static member function 
-		
-	double calcSalary();
-}; // end class Employee
+private:
+// INQUIRY
+	/** utility function to check name of employee.
+	* Only checks for alphabetic characters and first cap letter.
+	*
+	* @param testName The reference to the name to be tested.
+	*
+	* @return true if testName is valid, false otherwise.
+	*/
+	bool IsValidName(const string& testName)const;
+
+	/** utility function to check validity of NIC
+	* Only checks for alphanumeric characters and no of characters entered.
+	*
+	* @param testNIC The reference to the NIC to be tested.
+	*
+	* @return true if testNIC is valid, false otherwise.
+	*/
+	bool IsValidNIC(const string& testNIC)const;
+
+// DATA MEMBERS
+	string mName;
+	string mNIC;
+	const Date mBirthDate;
+	const Date mHireDate;
+	Address mHomeAddress;
+	double mTaxRate;
+	double mBasicSalary;
+	// class variable to count total no. of employees
+	static int msNoOfEmployees;		
+}; 
+// end class Employee
 
 #endif
+// _EMPLOYEE_H_
